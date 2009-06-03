b1a9de6a3cc3c1326dd57522337b68f9346e121b
Remco van 't Veer <rwvtveer@xs4all.nl>
2009-06-03 08:57:34 +0200

    renamed rails-test:current-test to rails-test:active-support-test-case-current-test

--- rails-test.el
+++ rails-test.el
@@ -240,7 +240,7 @@ Used when it's determined that the output buffer needs to be shown."
   (interactive)
   (let ((file (substring (buffer-file-name) (length (rails-project:root))))
         (method (rails-core:current-method-name))
-        (description (or (rails-test:current-test) (rails-shoulda:current-test))))
+        (description (or (rails-test:active-support-test-case-current-test) (rails-shoulda:current-test))))
     (when method
       (rails-test:run-single-file file (format "--name=%s" method)))
     (when description
