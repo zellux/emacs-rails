4596b13a466f411b7c11e4a298d85a474f00dc97
Remco van 't Veer <rwvtveer@xs4all.nl>
2009-07-29 20:13:06 +0200

    class renaming more general

--- rails-refactoring.el
+++ rails-refactoring.el
@@ -1,30 +1,47 @@
+(defun rails-refactoring:file (class &optional type)
+  (cond ((eq type :controller)
+         (rails-core:controller-file class))
+
+        ((eq type :functional-test)
+         (rails-core:functional-test-file class))
+
+        ((eq type :helper)
+         (rails-core:helper-file class))
+
+        ((eql type :helper-test)
+         (format "test/unit/helper/%s" (rails-core:file-by-class (concat class "HelperTest"))))
+        
+        ((eql type :views-dir)
+         (rails-core:views-dir class))
+
+        (t (error "not yet implemented type %s" type))))
+
+(defun rails-refactoring:file-exists-p (class &optional type)
+  (file-exists-p (rails-core:file (rails-refactoring:file class type))))
+
+(defun rails-refactoring:rename-class (from to &optional type)
+  (let ((from-file (rails-refactoring:file from type))
+        (to-file (rails-refactoring:file to type)))
+    (message "rename file from %s to %s" from-file to-file)
+    (rename-file (rails-core:file from-file) (rails-core:file to-file))
+    (let ((buffer (get-file-buffer (rails-core:file from-file))))
+      (when buffer (kill-buffer buffer)))
+
+    (message "change definition from %s to %s" from to)
+    (let ((buffer (get-file-buffer (rails-core:file to-file))))
+      (when buffer (kill-buffer buffer)))
+    (find-file (rails-core:file to-file))
+    (goto-char (point-min))
+    (if (re-search-forward (concat "^\\(class\\|module\\)[ \t]+" from) nil t)
+      (replace-match (concat "\\1 " to) nil nil)
+      (error "failed find class definition for renaming")))
+    (save-buffer))
+
 (defun rails-refactoring:controller-names ()
   (mapcar (lambda (name) (remove-postfix name "Controller")) (rails-core:controllers)))
 
 (defun rails-refactoring:current-controller-or-nil ()
   (condition-case nil (rails-core:current-controller) (error nil)))
-
-(defun rails-refactoring:rename-class-definition (from to)
-  (goto-char (point-min))
-  (if (re-search-forward (concat "^\\(class\\|module\\)[ \t]+" from) nil t)
-    (replace-match (concat "\\1 " to) nil nil)
-    (error "failed find class definition for renaming")))
-
-(defun rails-refactoring:rename-class-file (from-class from-file to-class to-file)
-  (message "rename file from %s to %s" from-file to-file)
-  (rename-file (rails-core:file from-file) (rails-core:file to-file))
-  (let ((buffer (get-file-buffer (rails-core:file from-file))))
-    (when buffer (kill-buffer buffer)))
-
-  (message "change definition from %s to %s" from-class to-class)
-  (find-file (rails-core:file to-file))
-  (rails-refactoring:rename-class-definition from-class to-class)
-  (save-buffer))
-
-(defun rails-refactoring:helper-test-file (controller)
-  (format "test/unit/helper/%s" (rails-core:file-by-class (concat controller "HelperTest"))))
-
-(assert (string= (rails-refactoring:helper-test-file "Users") "test/unit/helper/users_helper_test.rb"))
 
 (defun rails-refactoring:rename-controller (from to)
   (interactive (let* ((from (completing-read "Rename controller: "
@@ -46,37 +63,25 @@
     (when file (error "file '%s' already exists" file)))
 
   (message "refactoring controller class file")
-  (rails-refactoring:rename-class-file (rails-core:long-controller-name from)
-                                       (rails-core:controller-file from)
-                                       (rails-core:long-controller-name  to)
-                                       (rails-core:controller-file to))
+  (rails-refactoring:rename-class from to :controller)
 
-  (when (file-exists-p (rails-core:file (rails-core:views-dir from)))
-    (let ((from-dir (rails-core:views-dir from))
-          (to-dir (rails-core:views-dir to)))
+  (when (rails-refactoring:file-exists-p from :views-dir)
+    (let ((from-dir (rails-refactoring:file from :views-dir))
+          (to-dir (rails-refactoring:file to :views-dir)))
       (message "rename view directory from %s to %s" from-dir to-dir)
       (rename-file (rails-core:file from-dir) (rails-core:file to-dir))))
 
-  (when (file-exists-p (rails-core:file (rails-core:functional-test-file from)))
+  (when (rails-refactoring:file-exists-p from :functional-test)
     (message "refactoring functional test class file")
-    (rails-refactoring:rename-class-file (concat from "ControllerTest")
-                                         (rails-core:functional-test-file from)
-                                         (concat to "ControllerTest")
-                                         (rails-core:functional-test-file to)))
+    (rails-refactoring:rename-class from to :functional-test))
 
-  (when (file-exists-p (rails-core:file (rails-core:helper-file from)))
+  (when (rails-refactoring:file-exists-p from :helper)
     (message "refactoring helper class file")
-    (rails-refactoring:rename-class-file (concat from "Helper")
-                                         (rails-core:helper-file from)
-                                         (concat to "Helper")
-                                         (rails-core:helper-file to)))
+    (rails-refactoring:rename-class from to :helper))
 
-  (when (file-exists-p (rails-core:file (rails-refactoring:helper-test-file from)))
+  (when (rails-refactoring:file-exists-p from :helper-test)
     (message "refactoring helper test class file")
-    (rails-refactoring:rename-class-file (concat from "HelperTest")
-                                         (rails-refactoring:helper-test-file from)
-                                         (concat to "HelperTest")
-                                         (rails-refactoring:helper-test-file to)))
+    (rails-refactoring:rename-class from to :helper-test))
 
   (error "should rename layout")
 
